0f26751bb924756b31036f9172c633815f893602
HarshPatiL11 <patilharsh497@gmail.com>
2024-08-26 15:01:37 +0530

    Update userController.js

--- MVC/Controller/userController.js
+++ MVC/Controller/userController.js
@@ -137,28 +137,3 @@ export const updatePasswordController = async (req, res) => {
     });
   }
 };
-
-// Delete user
-export const userDelete = async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId);
-    if (user) {
-      await user.deleteOne();
-      return res.status(200).send({
-        success: true,
-        message: "User deleted successfully",
-      });
-    } else {
-      return res.status(404).send({
-        success: false,
-        message: "User not found",
-      });
-    }
-  } catch (error) {
-    console.error(`Error in API: ${error}`);
-    res.status(500).send({
-      status: "error",
-      message: "Internal Server Error || Error in delete API",
-    });
-  }
-};
