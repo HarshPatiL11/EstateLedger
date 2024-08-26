2990d0c2bf28f312a90285d05758826eff9d6475
HarshPatiL11 <patilharsh497@gmail.com>
2024-08-26 23:31:21 +0530

    Update ownerRouter.js

--- MVC/Routes/ownerRouter.js
+++ MVC/Routes/ownerRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   deleteProperty,
   registerProperty,
+  updateProperty,
 } from "../Controller/propertyController.js";
 import { ownerMiddleware } from "../Middleware/AdminMiddleware.js";
 
@@ -10,7 +11,10 @@ const ownerRouter = express.Router();
 // create property
 ownerRouter.post("/add-property", ownerMiddleware, registerProperty);
 
+// update property
+ownerRouter.put('/property/update/:id',ownerMiddleware,updateProperty)
+
 // property delete
-ownerRouter.delete("/delete/:id", ownerMiddleware, deleteProperty);
+ownerRouter.delete("/property/delete/:id", ownerMiddleware, deleteProperty);
 
 export default ownerRouter;
