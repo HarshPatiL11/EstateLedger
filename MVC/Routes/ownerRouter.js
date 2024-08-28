5f6078cd9b188d4f2abaa3b4d44ab16667141322
HarshPatiL11 <patilharsh497@gmail.com>
2024-08-28 14:25:27 +0530

    Update ownerRouter.js

--- MVC/Routes/ownerRouter.js
+++ MVC/Routes/ownerRouter.js
@@ -5,16 +5,35 @@ import {
   updateProperty,
 } from "../Controller/propertyController.js";
 import { ownerMiddleware } from "../Middleware/AdminMiddleware.js";
+import { authMiddle } from "../Middleware/AuthMiddleware.js";
+import formidable from "express-formidable";
 
 const ownerRouter = express.Router();
 
 // create property
-ownerRouter.post("/add-property", ownerMiddleware, registerProperty);
+ownerRouter.post(
+  "/property/add-property",
+  authMiddle,
+  ownerMiddleware,
+  formidable(),
+  registerProperty
+);
 
 // update property
-ownerRouter.put('/property/update/:id',ownerMiddleware,updateProperty)
+ownerRouter.put(
+  "/property/update/:id",
+  authMiddle,
+  ownerMiddleware,
+  formidable(),
+  updateProperty
+);
 
 // property delete
-ownerRouter.delete("/property/delete/:id", ownerMiddleware, deleteProperty);
+ownerRouter.delete(
+  "/property/delete/:id",
+  authMiddle,
+  ownerMiddleware,
+  deleteProperty
+);
 
 export default ownerRouter;
