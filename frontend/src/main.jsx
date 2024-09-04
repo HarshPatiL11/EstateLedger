30a9b9d2951f78b1c570da04cdca8284c0ac9b5b
HarshPatiL11 <patilharsh497@gmail.com>
2024-09-04 13:29:16 +0530

    removed bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,6 @@ import store from "./Components/Redux/Store.js";
 
 import App from "./App.jsx";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
