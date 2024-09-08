f0c4e0574e7b51ea314ea25f7af3c49249017040
HarshPatiL11 <patilharsh497@gmail.com>
2024-09-09 00:02:53 +0530

    Created A modal page and its css

---
+++ frontend/src/components/Pages/Modal.jsx
@@ -0,0 +1,20 @@
+// Modal.jsx
+import React from "react";
+import "../CSS/Modal.css"; // Add your CSS styles for the modal here
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="modal-close" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
